3340ded97ed8c7c70700adf0cbb454406b90d410
Nathan Dotz <nathan.dotz@gmail.com>
2011-07-12 12:34:14 -0400

    Add renderer into GLSurfaceView from main layout

--- src/com/backyardbrains/BackyardAndroidActivity.java
+++ src/com/backyardbrains/BackyardAndroidActivity.java
@@ -1,14 +1,23 @@
 package com.backyardbrains;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class BackyardAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-//    }
-    
+	private GLSurfaceView glSurface;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		this.glSurface = (GLSurfaceView) findViewById(R.id.glSurface);
+		if (this.glSurface != null) {
+			this.glSurface.setRenderer(new OpenGLRenderer());
+			this.glSurface.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+		}
+	}
+
 }
