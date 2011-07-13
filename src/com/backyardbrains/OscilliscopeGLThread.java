122ccea46fb29c0a5cc69ed7c7aaac69badbaa6b
Nathan Dotz <nathan.dotz@gmail.com>
2011-07-13 17:13:08 -0400

    setup GL and EGL objects in drawing thread

--- src/com/backyardbrains/OscilliscopeGLThread.java
+++ src/com/backyardbrains/OscilliscopeGLThread.java
@@ -1,25 +1,40 @@
 package com.backyardbrains;
 
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLDebugHelper;
+import android.opengl.GLU;
 import android.util.Log;
 import android.view.SurfaceView;
 
 public class OscilliscopeGLThread extends Thread {
-	
+
 	SurfaceView parent;
-	private boolean mDone = false; // signal whether thread is finished	
-	
+	private boolean mDone = false; // signal whether thread is finished
+	private EGL10 mEGL;
+	private EGLDisplay mGLDisplay;
+	private EGLConfig mGLConfig;
+	private EGLSurface mGLSurface;
+	private EGLContext mGLContext;
+	private GL10 mGL;
+
 	OscilliscopeGLThread(SurfaceView view) {
 		parent = view;
 	}
-	
+
 	public void run() {
 		initEGL();
 		initGL();
-		while(!mDone) {
+		while (!mDone) {
 			// draw things
 		}
 	}
-	
+
 	public void requestStop() {
 		mDone = true;
 		try {
@@ -28,21 +43,70 @@ public class OscilliscopeGLThread extends Thread {
 			Log.e("BYB", "GL Thread couldn't rejoin!", e);
 		}
 		cleanupGL();
-		
+
 	}
 
 	private void cleanupGL() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void initGL() {
-		// TODO Auto-generated method stub
+		// set viewport
+		int width = parent.getWidth();
+		int height = parent.getHeight();
+		mGL.glViewport(0,0,width,height);
+		
+		mGL.glMatrixMode(GL10.GL_PROJECTION);
+		mGL.glLoadIdentity();
+		// carry-over from GLUT
+		GLU.gluPerspective(mGL, 45f, (float)width/height, 1f, 30f);
+		
+		// Blackout, then we're ready to draw! \o/
+		mGL.glClearColor(0f, 0f, 0f, 1.0f);
 		
 	}
 
 	private void initEGL() {
-		// TODO Auto-generated method stub
-		
+		/**
+		 * Get EGL object, display object, and initialize
+		 */
+		mEGL = (EGL10) GLDebugHelper.wrap(EGLContext.getEGL(),
+				GLDebugHelper.CONFIG_CHECK_GL_ERROR
+						| GLDebugHelper.CONFIG_CHECK_THREAD, null);
+
+		mGLDisplay = mEGL.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+
+		// pass an array to eglInitialize to get the current GL Version
+		int[] curGLVersion = new int[2];
+		mEGL.eglInitialize(mGLDisplay, curGLVersion);
+
+		/**
+		 * Create a spec for the EGL config we'd like, then pass it to
+		 * ChooseConfig, which will populate our configs array with a closest
+		 * match
+		 */
+		int[] mConfigSpec = { EGL10.EGL_RED_SIZE, 5, EGL10.EGL_GREEN_SIZE, 6,
+				EGL10.EGL_BLUE_SIZE, 5, EGL10.EGL_DEPTH_SIZE, 16,
+				EGL10.EGL_NONE };
+
+		EGLConfig[] configs = new EGLConfig[1];
+		int[] num_config = new int[1];
+		mEGL.eglChooseConfig(mGLDisplay, mConfigSpec, configs, 1, num_config);
+		mGLConfig = configs[0];
+
+		/**
+		 * Finally, get a GL Surface to draw on, a context, blit them together,
+		 * and populate the GL object.
+		 */
+		mGLSurface = mEGL.eglCreateWindowSurface(mGLDisplay, mGLConfig,
+				parent.getHolder(), null);
+		mGLContext = mEGL.eglCreateContext(mGLDisplay, mGLConfig, EGL10.EGL_NO_CONTEXT, null);
+		mEGL.eglMakeCurrent(mGLDisplay, mGLSurface, mGLSurface, mGLContext);
+		mGL = (GL10) GLDebugHelper.wrap(
+				mGLContext.getGL(),
+				GLDebugHelper.CONFIG_CHECK_GL_ERROR
+				| GLDebugHelper.CONFIG_CHECK_THREAD, null);
+
 	}
 }
