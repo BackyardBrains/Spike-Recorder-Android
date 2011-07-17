d2dfca8f5f3a3133fe74802e138cc513f3ed0913
Nathan Dotz <nathan.dotz@gmail.com>
2011-07-17 17:40:34 -0400

    extract OscilliscopeGLSurfaceView

---
+++ src/com/backyardbrains/OscilliscopeGLSurfaceView.java
@@ -0,0 +1,44 @@
+package com.backyardbrains;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * OscilliscopeGLSurfaceView is a custom SurfaceView that implements
+ * callbacks for its own holder, and manages its own GL drawing thread.
+ * @author nate
+ *
+ */
+class OscilliscopeGLSurfaceView extends SurfaceView implements
+		SurfaceHolder.Callback {
+
+	SurfaceHolder mAndroidHolder;
+	private OscilliscopeGLThread mGLThread;
+
+	OscilliscopeGLSurfaceView(Context context) {
+		super(context);
+		mAndroidHolder = getHolder();
+		mAndroidHolder.addCallback(this);
+		mAndroidHolder.setType(SurfaceHolder.SURFACE_TYPE_GPU);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		mGLThread = new OscilliscopeGLThread(this);
+		mGLThread.start();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+
+	}
+}
