de9531b177965bb5056ea919b714a7746d1f431c
Nathan Dotz <nathan.dotz@gmail.com>
2011-07-17 18:32:37 -0400

    make more parent-class friendly

--- src/com/backyardbrains/MicListener.java
+++ src/com/backyardbrains/MicListener.java
@@ -7,14 +7,16 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder.AudioSource;
 import android.util.Log;
+import android.view.SurfaceView;
 
 public class MicListener extends Thread {
-	private boolean stopped = false;
+	private boolean mDone = false;
+	private SurfaceView parent;
 
-	private MicListener() {
+	MicListener(SurfaceView view) {
+		parent = view;
 		android.os.Process
 				.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-		start();
 	}
 
 	@Override
@@ -35,24 +37,24 @@ public class MicListener extends Thread {
 
 			recorder.startRecording();
 
-			// ... loop
-
-			while (!stopped) {
+			while (!mDone) {
 				short[] buffer = buffers[ix++ % buffers.length];
-
 				N = recorder.read(buffer, 0, buffer.length);
-
-				//process(buffer);
 			}
 		} catch (Throwable x) {
 			Log.w("MicListener", "Error reading voice audio", x);
 		} finally {
-			close();
+			requestStop();
 		}
 	}
 
-	private void close() {
-		stopped = true;
+	public void requestStop() {
+		mDone = true;
+		try {
+			join();
+		} catch (InterruptedException e) {
+			Log.e("BYB", "MicLIstener thread couldn't rejoin!", e);
+		}
 	}
 
 }
