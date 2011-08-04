a096af5d95d269e62f6cf720818bacf016863065
Nathan Dotz <nathan.dotz@gmail.com>
2011-08-04 12:23:36 -0400

    add notification to show that byb is polling audio

--- src/com/backyardbrains/AudioService.java
+++ src/com/backyardbrains/AudioService.java
@@ -2,6 +2,9 @@ package com.backyardbrains;
 
 import java.nio.ByteBuffer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -24,6 +27,9 @@ public class AudioService extends Service implements RecievesAudio {
 
 	private BackyardBrainsApplication app;
 	private Thread micThread;
+
+	private int NOTIFICATION = R.string.mic_thread_running;
+	private NotificationManager mNM;
 
 	/*
 	 * (non-Javadoc)
@@ -70,18 +76,31 @@ public class AudioService extends Service implements RecievesAudio {
 		};
 		micThread.start();
 		Log.d(TAG, "Mic thread started");
+		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		showNotification();
 	}
-	
+
+	private void showNotification() {
+		CharSequence text = getText(R.string.mic_thread_running);
+		Notification not = new Notification(R.drawable.ic_launcher_byb, text,
+				System.currentTimeMillis());
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, BackyardAndroidActivity.class), 0);
+		not.setLatestEventInfo(this, "Backyard Brains", text, contentIntent);
+		mNM.notify(NOTIFICATION, not);
+	}
+
 	public void turnOffMicThread() {
 		Log.d(TAG, "Mic Thread Shut Off");
 		this.mic.requestStop();
 		this.mic = null;
+		mNM.cancel(NOTIFICATION);
 	}
 
 	public ByteBuffer getAudioFromMicListener() {
 		return mic.getAudioInfo();
 	}
-	
+
 	@Override
 	public void receiveAudio(ByteBuffer audioData) {
 		Log.i(TAG, "Got audio data");
