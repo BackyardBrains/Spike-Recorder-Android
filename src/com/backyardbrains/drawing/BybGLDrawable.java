b1a4c6c474265ffb695656d87c6c10286a058631
Nathan Dotz <nathan.dotz@gmail.com>
2011-08-08 11:17:27 -0400

    Provide Javadoc compatible documentation

--- src/com/backyardbrains/drawing/BybGLDrawable.java
+++ src/com/backyardbrains/drawing/BybGLDrawable.java
@@ -8,25 +8,52 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.util.Log;
 
+/**
+ * An object capable of drawing itself to a provided GL10 object
+ * 
+ * @author Nathan Dotz <nate@backyardbrains.com>
+ * @version 1
+ * 
+ */
 class BybGLDrawable {
+	/**
+	 * Tag for use in LogCat
+	 */
 	private static final String TAG = "BYBGLShape";
 
+	/**
+	 * Scaling in Y domain to be used when drawing on a GL surface
+	 */
 	private final float Y_SCALING = .001f;
 
 	/**
-	 * 
+	 * Reference to the parent thread responsible for maintaining this object.
 	 */
 	private final OscilliscopeGLThread parent;
 
 	/**
+	 * Construct a drawable object, storing a reference to the parent thread.
+	 * 
 	 * @param oscilliscopeGLThread
+	 *            the thread responsible for this object.
 	 */
 	BybGLDrawable(OscilliscopeGLThread oscilliscopeGLThread) {
 		parent = oscilliscopeGLThread;
 	}
 
+	/**
+	 * The array of 16-bit numbers that represents the data to be drawn
+	 */
 	private short[] mBufferToDraw;
 
+	/**
+	 * Takes an array of floats and returns a buffer representing the same
+	 * floats
+	 * 
+	 * @param array
+	 *            to be converted
+	 * @return converted array as FloatBuffer
+	 */
 	FloatBuffer getFloatBufferFromFloatArray(float array[]) {
 		ByteBuffer temp = ByteBuffer.allocateDirect(array.length * 4);
 		temp.order(ByteOrder.nativeOrder());
@@ -36,6 +63,11 @@ class BybGLDrawable {
 		return buf;
 	}
 
+	/**
+	 * Draw this object on the provided {@link GL10} object.
+	 * 
+	 * @param gl_obj
+	 */
 	public void draw(GL10 gl_obj) {
 		// vertices = transform(vertices);
 		// FloatBuffer mVertexBuffer = getFloatBufferFromFloatArray(vertices);
@@ -45,6 +77,16 @@ class BybGLDrawable {
 		gl_obj.glDrawArrays(GL10.GL_LINE_STRIP, 0, mVertexBuffer.limit() / 3);
 	}
 
+	/**
+	 * Convert the local buffer-to-draw data over to a {@link FloatBuffer}
+	 * structure suitable for feeding to
+	 * {@link GL10#glDrawArrays(int, int, int)}
+	 * 
+	 * @param shortArrayToDraw
+	 *            containing raw audio data
+	 * @return {@link FloatBuffer} ready to be fed to
+	 *         {@link GL10#glDrawArrays(int, int, int)}
+	 */
 	private FloatBuffer getWaveformBuffer(short[] shortArrayToDraw) {
 		if (shortArrayToDraw == null) {
 			Log.w(TAG, "Drawing fake line with null data");
@@ -65,6 +107,13 @@ class BybGLDrawable {
 		return getFloatBufferFromFloatArray(arr);
 	}
 
+	/**
+	 * Called from the parent thread, this takes a {@link ByteBuffer} of audio
+	 * data from the recording device and converts it into an array of 16-bit
+	 * shorts, to later be processed by the drawing functions.
+	 * 
+	 * @param audioBuffer {@link ByteBuffer} to be drawn
+	 */
 	public void setBufferToDraw(ByteBuffer audioBuffer) {
 		if (audioBuffer != null) {
 			audioBuffer.clear();
