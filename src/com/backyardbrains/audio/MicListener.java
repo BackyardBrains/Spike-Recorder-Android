8b9d9187eac9f596dd7bd129a20caa4780310fd6
Nathan Dotz <nathan.dotz@gmail.com>
2011-08-08 14:33:56 -0400

    provide javadoc for all of c.byb.audio

--- src/com/backyardbrains/audio/MicListener.java
+++ src/com/backyardbrains/audio/MicListener.java
@@ -3,22 +3,60 @@ package com.backyardbrains.audio;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder.AudioSource;
 import android.util.Log;
 
+/**
+ * A specialized thread to manage Android's {@link AudioRecord} objects and
+ * continuously poll out information to an accompanying {@link RecievesAudio}
+ * interface.
+ * 
+ * @author Nathan Dotz <nate@backyardbrains.com>
+ * 
+ */
 public class MicListener extends Thread {
+	/**
+	 * Tag to use when logging
+	 */
 	private static final String TAG = "BYBMicListener";
 
+	/**
+	 * Set sample rate
+	 * 
+	 * @TODO turn this into a list of sample rates to loop over
+	 */
 	private static final int sampleRate = 44100;
+	/**
+	 * see if we're finished yet
+	 */
 	private boolean mDone = false;
+	/**
+	 * @see android.media.AudioRecord
+	 */
 	private AudioRecord recorder;
+	/**
+	 * Buffer to store raw microphone/input data in. allocated in
+	 * {@link MicListener#MicListener()} and used while polling
+	 * {@link AudioRecord} in {@link MicListener#run()}
+	 */
 	private ByteBuffer audioInfo;
+	/**
+	 * placeholder for a service that implements {@link RecievesAudio}
+	 */
 	private RecievesAudio service;
+	/**
+	 * buffer size appropriate for this device (set in
+	 * {@link MicListener#MicListener()}
+	 */
 	private int buffersize;
 
+	/**
+	 * Find the appropriate buffer size for working on this device and allocate
+	 * space for the audioInfo {@link ByteBuffer} based on that size, then tell
+	 * Android we'll be using high-priority audio-processing.
+	 */
 	MicListener() {
 		buffersize = AudioRecord.getMinBufferSize(sampleRate,
 				AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
@@ -29,12 +67,25 @@ public class MicListener extends Thread {
 	}
 
 	/**
+	 * Called by {@link AudioService#getAudioFromMicListener()} to pull up
+	 * {@link ByteBuffer}
+	 * 
 	 * @return the audioInfo
 	 */
 	public ByteBuffer getAudioInfo() {
 		return audioInfo;
 	}
 
+	/**
+	 * An alternate to {@link Thread#start()}, which allows us to provide a
+	 * service which implements {@link RecievesAudio} then start the thread as
+	 * normal. This allows for {@link MicListener#run()} to poll the
+	 * implementing interface with new data from {@link AudioRecord} as it
+	 * becomes available.
+	 * 
+	 * @param svc
+	 *            the service that implements the {@link RecievesAudio}
+	 */
 	public void start(RecievesAudio svc) {
 		service = svc;
 		if (service != null) {
@@ -45,6 +96,14 @@ public class MicListener extends Thread {
 		super.start();
 	}
 
+	/**
+	 * Get a new recorder, check to see that we can actually record, start
+	 * recording, and then continuously read audio from the recording interface,
+	 * while pushing it out to the receiving service that implements
+	 * {@link RecievesAudio} (set in {@link MicListener#start(RecievesAudio)}.
+	 * 
+	 * @see java.lang.Thread#run()
+	 */
 	public void run() {
 		Log.d(TAG, "Thread Launched");
 		recorder = null;
@@ -77,12 +136,21 @@ public class MicListener extends Thread {
 		}
 	}
 
+	/**
+	 * Convenience function for spitting out new {@link AudioRecord} objects
+	 * 
+	 * @return an {@link AudioRecord} object preset to match the rest of the
+	 *         application
+	 */
 	private AudioRecord newRecorder() {
 		return new AudioRecord(AudioSource.DEFAULT, sampleRate,
 				AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
 				buffersize * 4);
 	}
 
+	/**
+	 * clean up {@link AudioRecord} resource before exiting thread.
+	 */
 	public void requestStop() {
 		mDone = true;
 		if (recorder != null) {
--- src/com/backyardbrains/audio/RecievesAudio.java
+++ src/com/backyardbrains/audio/RecievesAudio.java
@@ -2,6 +2,21 @@ package com.backyardbrains.audio;
 
 import java.nio.ByteBuffer;
 
+import android.app.Service;
+
+/**
+ * A simple interface to attach to services which allows a callback into
+ * android's built-in classes from an unrelated thread
+ * 
+ * @author Nathan Dotz <nate@backyardbrains.com>
+ * 
+ */
 public interface RecievesAudio {
+	/**
+	 * Called by a thread to pass audio {@link ByteBuffer} into a service. The
+	 * service should then do as it sees fit with the data.
+	 * 
+	 * @param audioData
+	 */
 	public void receiveAudio(ByteBuffer audioData);
 }
