7bce508c7771b7a9f1889e777c3e3dcf93192aae
Nathan Dotz <nathan.dotz@gmail.com>
2011-10-13 16:59:42 -0400

    Fix wave file length by moving to DataOutputStream that corrects for endianness

--- src/com/backyardbrains/audio/RecordingSaver.java
+++ src/com/backyardbrains/audio/RecordingSaver.java
@@ -83,13 +83,14 @@ public class RecordingSaver implements ReceivesAudio {
 	}
 
 	private class ConvertToWavefile extends AsyncTask<File, Void, String> {
-
+		
 		private void convertToWave(File mFileToRecordTo2) throws IOException {
 			// TODO Auto-generated method stub
 			FileInputStream in = new FileInputStream(mFileToRecordTo2);
 			File outputFile = new File(mFileToRecordTo2.getAbsolutePath()
 					+ ".wav");
 			FileOutputStream out = new FileOutputStream(outputFile);
+			DataOutputStream datastream = new DataOutputStream(out);
 
 			int mSampleRate = 44100;
 			int mChannels = 1;
@@ -99,71 +100,33 @@ public class RecordingSaver implements ReceivesAudio {
 					* mChannels * (mBitsPerSample / 2);
 
 			long chunksize = subchunk2size + 36;
-			long longSampleRate = mSampleRate;
-			long byteRate = mSampleRate * mChannels * (mBitsPerSample / 2);
+			int byteRate = mSampleRate * mChannels * mBitsPerSample / 8;
 
-			byte[] header = new byte[44];
-			header[0] = 'R'; // RIFF/WAVE header
-			header[1] = 'I';
-			header[2] = 'F';
-			header[3] = 'F';
-			header[4] = (byte) (chunksize & 0xff);
-			header[5] = (byte) ((chunksize >> 8) & 0xff);
-			header[6] = (byte) ((chunksize >> 16) & 0xff);
-			header[7] = (byte) ((chunksize >> 24) & 0xff);
-			header[8] = 'W';
-			header[9] = 'A';
-			header[10] = 'V';
-			header[11] = 'E';
-			header[12] = 'f'; // 'fmt ' chunk
-			header[13] = 'm';
-			header[14] = 't';
-			header[15] = ' ';
-			header[16] = 16; // 4 bytes: size of 'fmt ' chunk
-			header[17] = 0;
-			header[18] = 0;
-			header[19] = 0;
-			header[20] = 1; // format = 1
-			header[21] = 0;
-			header[22] = (byte) mChannels;
-			header[23] = 0;
-			header[24] = (byte) (longSampleRate & 0xff);
-			header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-			header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-			header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-			header[28] = (byte) (byteRate & 0xff);
-			header[29] = (byte) ((byteRate >> 8) & 0xff);
-			header[30] = (byte) ((byteRate >> 16) & 0xff);
-			header[31] = (byte) ((byteRate >> 24) & 0xff);
-			header[32] = (byte) (mChannels * mBitsPerSample / 8); // block align
-			header[33] = 0;
-			header[34] = 16; // bits per sample
-			header[35] = 0;
-			header[36] = 'd';
-			header[37] = 'a';
-			header[38] = 't';
-			header[39] = 'a';
-			header[40] = (byte) (subchunk2size & 0xff);
-			header[41] = (byte) ((subchunk2size >> 8) & 0xff);
-			header[42] = (byte) ((subchunk2size >> 16) & 0xff);
-			header[43] = (byte) ((subchunk2size >> 24) & 0xff);
-			out.write(header, 0, 44);
-
-			// byte[] buffer = new byte[(int) mFileToRecordTo2.length()];
-			// in.read(buffer);
-			// out.write(buffer);
-
+			datastream.writeBytes("RIFF");
+			datastream.writeInt(Integer.reverseBytes((int) chunksize));
+			datastream.writeBytes("WAVEfmt ");
+			datastream.writeInt(Integer.reverseBytes(16));
+			datastream.writeShort(Short.reverseBytes((short) 1));
+			datastream.writeShort(Short.reverseBytes((short) mChannels));
+			datastream.writeInt(Integer.reverseBytes(mSampleRate));
+			datastream.writeInt(Integer.reverseBytes(byteRate));
+			datastream.writeShort(Short.reverseBytes((short)(mChannels*mBitsPerSample/8))); // block align
+			datastream.writeShort(Short.reverseBytes((short) mBitsPerSample));
+			datastream.writeBytes("data");
+			datastream.writeInt(Integer.reverseBytes((int) subchunk2size));
+			
 			byte tempA;
 			byte tempB;
 			long counter = 0;
 			while (counter < mFileToRecordTo2.length() && in.available() > 0) {
 				tempA = (byte) in.read();
 				tempB = (byte) in.read();
-				out.write(tempB);
-				out.write(tempA);
+				datastream.write(tempB);
+				datastream.write(tempA);
 			}
 			in.close();
 			out.close();
+			datastream.close();
 		}
 
 		@Override
