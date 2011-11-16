628f029b9d1c2200dd8b2a62babaa4042ce1a72c
Nathan Dotz <nathan.dotz@gmail.com>
2011-11-16 14:22:26 -0500

    Add logging for when we actually create a threshold thread

--- src/com/backyardbrains/drawing/TriggerViewThread.java
+++ src/com/backyardbrains/drawing/TriggerViewThread.java
@@ -1,10 +1,15 @@
 package com.backyardbrains.drawing;
 
+import android.util.Log;
+
 public class TriggerViewThread extends OscilloscopeGLThread {
+
+	private static final String TAG = Class.class.getCanonicalName();
 
 	TriggerViewThread(OscilloscopeGLSurfaceView view) {
 		super(view);
+		Log.d(TAG , "Creating TriggerViewThread");
 	}
 
-	public final boolean drawThreshholdLine = true;
+	public final boolean drawThresholdLine = true;
 }
