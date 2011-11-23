99ab9e67c7b0ccf10455e5a805e137d279350624
Nathan Dotz <nathan.dotz@gmail.com>
2011-11-23 17:49:58 -0500

    Fix unused imports

--- src/com/backyardbrains/BackyardAndroidActivity.java
+++ src/com/backyardbrains/BackyardAndroidActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
