8868fea10e397e8593c2824af2903390ba531e9a
Nathan Dotz <nathan.dotz@gmail.com>
2011-12-13 14:36:59 -0500

    Add finger-tracking to control the threshold line

--- src/com/backyardbrains/drawing/TriggerViewThread.java
+++ src/com/backyardbrains/drawing/TriggerViewThread.java
@@ -6,22 +6,24 @@ import java.nio.ShortBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import android.util.Log;
 
 public class TriggerViewThread extends OscilloscopeGLThread {
 
 	private static final String TAG = Class.class.getCanonicalName();
-	private float thresholdValue;
+	private float thresholdPixelHeight;
 
 	TriggerViewThread(OscilloscopeGLSurfaceView view) {
 		super(view);
-		Log.d(TAG , "Creating TriggerViewThread");
+		Log.d(TAG, "Creating TriggerViewThread");
 	}
 
 	public boolean isDrawThresholdLine() {
 		return true;
 	}
-	
+
 	/**
 	 * Initialize GL bits, set up the GL area so that we're lookin at it
 	 * properly, create a new {@link BybGLDrawable}, then commence drawing on it
@@ -35,7 +37,8 @@ public class TriggerViewThread extends OscilloscopeGLThread {
 
 		bindAudioService(true);
 		registerScaleChangeReceiver(true);
-		setThresholdValue();
+		registerThresholdChangeReceiver(true);
+		setDefaultThresholdValue();
 		while (!mDone) {
 			// grab current audio from audioservice
 			if (mAudioServiceIsBound) {
@@ -73,7 +76,6 @@ public class TriggerViewThread extends OscilloscopeGLThread {
 					setGlWindow(samplesToShow);
 					waveformShape.draw(glman.getmGL());
 					if (isDrawThresholdLine()) {
-						setThresholdValue();
 						drawThresholdLine();
 					}
 					glman.swapBuffers();
@@ -87,20 +89,40 @@ public class TriggerViewThread extends OscilloscopeGLThread {
 		}
 		bindAudioService(false);
 		registerScaleChangeReceiver(false);
+		registerThresholdChangeReceiver(false);
 		mConnection = null;
 	}
 
-	private void setThresholdValue() {
-		thresholdValue = (yEnd / 2 / mScaleFactor);
-		float yPerDiv = thresholdValue /  24.5f;
-		parent.setmVText(yPerDiv);
-		Log.d(TAG, "ThresholdValue is "+thresholdValue);
+	protected void setLabels(int samplesToShow) {
+		setmVText();
+		super.setLabels(samplesToShow);
 	}
-	
+
+	private void setDefaultThresholdValue() {
+		thresholdPixelHeight = glHeightToPixelHeight(yEnd / 2 / mScaleFactor);
+		setmVText();
+	}
+
+	private void setmVText() {
+		float yPerDiv = pixelHeightToGlHeight(thresholdPixelHeight) / 24.5f;
+		parent.setmVText(yPerDiv);
+	}
+
+	private float glHeightToPixelHeight(float glHeight) {
+		return (glHeight / (yEnd / mScaleFactor * 2)) * parent.getHeight();
+	}
+
+	private float pixelHeightToGlHeight(float pxHeight) {
+		return pxHeight / parent.getHeight() * (yEnd / mScaleFactor * 2);
+	}
+
 	protected void drawThresholdLine() {
-		float[] thresholdLine = new float[] {
-			0, thresholdValue, getxEnd(), thresholdValue
-		};
+		// Log.d(TAG, "ThresholdValue is "+thresholdValue +
+		// " - drew threshold line at " +
+		// glHeightToPixelHeight(thresholdValue));
+
+		float[] thresholdLine = new float[] { 0, getThresholdValue(),
+				getxEnd(), getThresholdValue() };
 		FloatBuffer thl = getFloatBufferFromFloatArray(thresholdLine);
 		glman.getmGL().glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		glman.getmGL().glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
@@ -108,6 +130,41 @@ public class TriggerViewThread extends OscilloscopeGLThread {
 		glman.getmGL().glVertexPointer(2, GL10.GL_FLOAT, 0, thl);
 		glman.getmGL().glDrawArrays(GL10.GL_LINES, 0, 4);
 		glman.getmGL().glDisableClientState(GL10.GL_VERTEX_ARRAY);
+	}// ((?!GC_)).*
+
+	public float getThresholdValue() {
+		return pixelHeightToGlHeight(thresholdPixelHeight);
+	}
+
+	public float getThresholdYValue() {
+		return parent.getHeight()/2 - thresholdPixelHeight;
+	}
+
+	public void adjustThresholdValue(float dy) {
+		if (dy < parent.getHeight() / 2)
+			thresholdPixelHeight = parent.getHeight() / 2 - dy;
+	}
+
+	protected void registerThresholdChangeReceiver(boolean on) {
+		if (on) {
+			thChangeReceiver = new ThresholdChangeReceiver();
+			parent.getContext().registerReceiver(thChangeReceiver,
+					new IntentFilter("BYBThresholdChange"));
+		} else {
+
+			parent.getContext().unregisterReceiver(thChangeReceiver);
+		}
+
+	}
+
+	private ThresholdChangeReceiver thChangeReceiver;
+
+	private class ThresholdChangeReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(android.content.Context context,
+				android.content.Intent intent) {
+			adjustThresholdValue(intent.getFloatExtra("deltathreshold", 1));
+		};
 	}
 
 }
