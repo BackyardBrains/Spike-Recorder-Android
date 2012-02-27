00cfa274cda995b4235661b951ad06eb4965a751
Nathan Dotz <nathan.dotz@gmail.com>
2012-02-27 13:46:22 -0500

    add the actual activity

---
+++ src/com/backyardbrains/TriggerActivity.java
@@ -0,0 +1,19 @@
+package com.backyardbrains;
+
+import com.backyardbrains.drawing.ThresholdGlSurfaceView;
+import com.backyardbrains.view.UIFactory;
+
+public class TriggerActivity extends BackyardAndroidActivity {
+
+	@Override
+	protected void setGlSurface() {
+		// TODO Auto-generated method stub
+		mAndroidSurface = new ThresholdGlSurfaceView(this);
+	}
+	
+	@Override
+	protected void enableUiForActivity() {
+		UIFactory.hideRecordingButtons(this);
+		UIFactory.showSampleSliderBox(this);
+	}
+}
