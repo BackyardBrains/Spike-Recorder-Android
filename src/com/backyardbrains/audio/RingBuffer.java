974f61dcf45591d27377aa76ce2a85b027d870c6
Nathan Dotz <nathan.dotz@gmail.com>
2012-06-09 14:50:53 -0400

    Update to remove as many redundant copies as possible,

--- src/com/backyardbrains/audio/RingBuffer.java
+++ src/com/backyardbrains/audio/RingBuffer.java
@@ -28,36 +28,12 @@ public class RingBuffer {
 		beginning = end = -1;
 	}
 
-	public void addEnd(short r) {
-		if (end >= 0) {
-			// not empty!
-			++end;
-			if (end >= buffer.length)
-				end = 0;
-			if (end == beginning)
-				beginning++;
-			if (beginning >= buffer.length)
-				beginning = 0;
-		} else {
-			beginning = end = 0;
-		}
-		buffer[end] = r;
-	}
-
-	public void add(ByteBuffer incoming) {
-		/*
-		byte swp;
-		while (buffer.hasRemaining()) {
-			swp = buffer.get();
-			addEnd(buffer.get());
-			addEnd(swp);
-		}
-		*/
+	public void add(final ByteBuffer incoming) {
 		incoming.clear();
-		ShortBuffer sb = incoming.asShortBuffer();
-		while (sb.hasRemaining()) {
-			addEnd(sb.get());
-		}
+		final ShortBuffer sb = incoming.asShortBuffer();
+		System.arraycopy(buffer, sb.capacity(), buffer, 0, buffer.length-sb.capacity());
+		sb.get(buffer, buffer.length-sb.capacity(), sb.capacity());
+		//System.arraycopy(s, 0, buffer, buffer.length-s.length, s.length);
 	}
 
 	/**
@@ -66,23 +42,9 @@ public class RingBuffer {
 	 * @return
 	 */
 	public short[] getArray() {
-		if (beginning == 0) {
-			short[] returnArray = new short[end + 1];
-			System.arraycopy(buffer, 0, returnArray, 0, returnArray.length);
-			return returnArray;
-		}
-		short[] returnArray = new short[buffer.length];
-		System.arraycopy(buffer, beginning, returnArray, 0, buffer.length
-				- beginning);
-		System.arraycopy(buffer, 0, returnArray, buffer.length - beginning,
-				end + 1);
-		return returnArray;
+		return buffer;
 	}
 
 	public void zeroFill() {
-		for (int i = 0; i < buffer.length; i++) {
-			addEnd((short) 0);
-		}
-
 	}
 }
