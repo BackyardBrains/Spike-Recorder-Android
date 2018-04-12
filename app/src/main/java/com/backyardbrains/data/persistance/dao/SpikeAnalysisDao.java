24a212556d5b100e25dd47df135391274fc73771
Tihomir Leka <ticapeca@gmail.com>
2018-04-12 11:10:49 +0200

    After project structure change merge (limbo state)

---
+++ app/src/main/java/com/backyardbrains/data/persistance/dao/SpikeAnalysisDao.java
@@ -0,0 +1,22 @@
+package com.backyardbrains.data.persistance.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.support.annotation.NonNull;
+import com.backyardbrains.data.persistance.entity.SpikeAnalysis;
+
+/**
+ * @author Tihomir Leka <ticapeca at gmail.com.
+ */
+@Dao public interface SpikeAnalysisDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE) long insertSpikeAnalysis(@NonNull SpikeAnalysis analysis);
+
+    @Query("SELECT * FROM spike_analysis WHERE file_path = :filePath") SpikeAnalysis loadSpikeAnalysis(
+            @NonNull String filePath);
+
+    @Query("SELECT id FROM spike_analysis WHERE file_path = :filePath") long loadSpikeAnalysisId(
+            @NonNull String filePath);
+}
